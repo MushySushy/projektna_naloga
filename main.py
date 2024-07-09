e15b8919fc3efc275498edf8fcbbed456a273089
MushySushy <lukamshluka@gmail.com>
2024-07-09 12:36:42 +0200

    Omogocil popolnoma avtomatično nabiranje podatkov, nekoliko uredil kodo

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
+import web_stvarce as ws
 
 
 def main():
-    pass
+    ws.naberi_kljuce(1000,"podatki.csv")
 
 
 if __name__=="__main__":
--- web_stvarce.py
+++ web_stvarce.py
@@ -30,42 +30,47 @@ format_strings=(
 
 def html2podatki(s):
     t=[(lambda x:x.groups()[0] if x else None)(re.search(f,s,flags=re.DOTALL)) for f in format_strings]
-    t[3]=t[3].replace(" ","")
-    t[5]=t[5].replace(" ","")
-    t[4]=(lambda x:int(x[0])*60+int(x[1]))(re.match("(\d+)h (\d+)m",t[4]).groups())
+
+    if t[3]!=None:
+        t[3]=t[3].replace(" ","")
+    if t[4]!=None:
+        t[4]=sum(int(i[:-1])*{"s":1,"m":60,"h":60*60,"d":24*60*60}[i[-1]] for i in t[4].split(" "))
+    if t[5]!=None:
+        t[5]=t[5].replace(" ","")
+    
     for i in range(2,len(t)):
-        t[i]=int(t[i])
+        if t[i]!=None:
+            t[i]=int(t[i])
 
     return tuple(t)
 
-        
 
 def naberi_podatke_igralca(k):
     for i in range(3):
         try:
             s=seja.get("https://minesweeper.online/player/"+str(k),timeout=1)
             #print(s.text,s)
-            if "Trophies" not in s.text:# or "    " in s.text:
+            if "Trophies" not in s.text:
                 raise Exception()
             return (int(k),)+html2podatki(s.text)
         except Exception:
             pass
 
 
-def naberi_kljuce(n):
+def naberi_kljuce(n,file):
     o=Options()
     #o.add_argument("--headless")
     d=webdriver.Chrome(options=o)
     d.implicitly_wait(5)
     d.get("https://minesweeper.online/best-players")
     time.sleep(1)
-    #d.find_element(By.ID,"show_all").click()
 
-    if os.path.exists("kljuci.txt"):
-        os.remove("kljuci.txt")
-    f=open("kljuci.txt","a")
-    
-    start=time.time()
+    if os.path.exists(file):
+        os.remove(file)
+    f=open(file,"a",encoding="UTF-8")
+    f.write("kljuc,ime,drzava,pokali,zmage,endurance,exp,cas1,cas2,cas3,eff1,eff2,eff3,mastery1,mastery2,mastery3,ws1,ws2,ws3\n")
+
+    start_time=time.time()
 
     for i in range(1,n//10+1):
         while 1:
@@ -73,36 +78,25 @@ def naberi_kljuce(n):
                 s=d.find_element(By.ID,"stat_table_body").get_attribute("innerHTML")
                 d.find_element(By.LINK_TEXT,">").click()
                 for j in zapisovanje.izlusci_private_key(s):
-                    f.write(j+"\n")
-                    #print(naberi_podatke_igralca(j))
-            
+                    try:
+                        p=",".join(str(k) for k in naberi_podatke_igralca(j))+"\n"
+                        f.write(p)
+                        print(p,end="")
+                    except Exception:
+                        print("Zgubil igralca ",j)
             except StaleElementReferenceException:
+                print("Error pri ",i)
                 continue
             break
-        print("Nabral %d/%d igralcev (%.2f%%)"%(i*10,n,i/n*1000))
-    
-    print("Trajalo %.1f sekund"%(time.time()-start))
+        print("\nNabral %d/%d igralcev (%.2f%%) v %.1f sekundah\n"%(i*10,n,i/n*1000,time.time()-start_time))
 
     f.close()
 
 def main():
-    #print(seja.get("https://minesweeper.online/player/"+str(9740766),timeout=1).text)
-    #return
-    #s=open("test.html",encoding="UTF-8").read()
-    #print(s)
-    #print(html2podatki(s))
-    #return
-    
-    #naberi_kljuce(1000)
-    #print(naberi_podatke_igralca(2620977))
-    #return
-    k=tuple(open("kljuci.txt").read().split("\n"))[:200]
-
-    for i in k:
-        print(naberi_podatke_igralca(i))
+    naberi_kljuce(1000,"bepis.csv")
+    #print(naberi_podatke_igralca(6596922))
+    #print(html2podatki(open("test.html",encoding="UTF-8").read()))
 
 
 if __name__=="__main__":
-    start=time.time()
     main()
-    print("Trajalo %.1f sekund"%(time.time()-start))
